190f1f94df256792a851cebe0fd618ec575b0933
Mayank bisht <mayankbisht8126@gmail.com>
2023-10-12 21:24:57 +0530

    While.java

---
+++ While.java
@@ -0,0 +1,12 @@
+public class While{
+	public static void main(String [] args){
+		String Al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		j=0;
+		i = 26;
+		While(j<=i){
+			System.out.println(Al[j]);
+		}
+	}
+}
+
+ 
