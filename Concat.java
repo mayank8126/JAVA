2a1cfa586e59c441d9d69d573f63e28140042444
Mayank bisht <mayankbisht8126@gmail.com>
2023-10-13 22:17:10 +0530

    Concat.java

---
+++ Concat.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Concat{
+	public static void main(String [] args){
+		Scanner obj= new Scanner(System.in);
+		System.out.println("Enter Your name -");
+		String name= obj.nextLine();
+		System.out.println("Enter Your age -");
+		int age=obj.nextInt();
+		System.out.println("Your name is "+name+" and age - "+age);
+	}
+}
+
+		
