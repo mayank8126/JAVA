03e36b33c13690e1091df6faf06e8f13f67b06c9
Mayank bisht <mayankbisht8126@gmail.com>
2023-10-15 21:02:34 +0530

    Import.java

---
+++ Basic/Import.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Import{
+	public static void main(String [] args){
+		Scanner myobj = new Scanner(System.in);
+		System.out.println("Enter your name - ");
+		String name = myobj.nextLine();
+		System.out.println(name);
+	}
+}
